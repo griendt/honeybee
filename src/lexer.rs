da50e21ad6b7656e5142f03ea28cd6351305b8b7
Alex van de Griendt <alex@e-cube.nl>
2020-05-19 22:24:30 +0200

    Support for lexing numerics

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,8 @@
 use core::panicking::panic_fmt;
+use std::process::exit;
 
+use crate::io::error;
+use crate::lexer::State::Operator;
 use crate::token::{Token, TokenType};
 
 const ENCAPSULATORS_LEFT: [char; 4] = [
@@ -16,8 +19,10 @@ const ENCAPSULATORS_RIGHT: [char; 4] = [
     '>',
 ];
 
-const OPERATORS: [char; 1] = [
+const OPERATORS: [char; 3] = [
     '=',
+    '!',
+    '-',
 ];
 
 const SEPARATORS: [char; 2] = [
@@ -45,6 +50,8 @@ enum State {
     StringEscape,
     // A numeric value
     Numeric,
+    // The decimal dot
+    DecimalSeparator,
     // A number after a decimal dot
     NumericDecimal,
     // An atom or some operator using the colon symbol
@@ -71,14 +78,17 @@ impl Lexer {
     pub fn lex(&mut self, character: char) -> () {
         let new_state = self.get_next_state(character);
 
-        if new_state == self.state || self.state == State::None
+        if new_state == self.state
+            || self.state == State::None
+            || (self.state == State::Numeric && new_state == State::DecimalSeparator)
+            || (self.state == State::DecimalSeparator && new_state == State::NumericDecimal)
         {
-            // We remain in the same state, or we enter any state from the None state.
+            // We remain in the same state, or we enter any state from the None state,
+            // or we are parsing a decimal number.
             // This means we can append the character to
             // our register and move on to the next character.
             self.register.push(character);
-        }
-        else {
+        } else {
             // We are switching states. In every case, that means we have to flush the
             // current register according to the current state. Then we can switch
             // safely and start the new token with a fresh register.
@@ -105,12 +115,15 @@ impl Lexer {
 
     fn end_token(&mut self) {
         let token_type = match self.state {
+            State::Separator => TokenType::Separator,
+            State::Operator => TokenType::Operator,
+            State::Numeric | State::NumericDecimal | State::DecimalSeparator => TokenType::Numeric,
             State::Identifier => TokenType::Identifier,
             State::None => TokenType::None,
 
             // For each state, we must determine what type of token
             // we should add as the end the current token.
-            _ => unimplemented!(),
+            _ => unimplemented!("{:?}", self.state),
         };
 
         if token_type != TokenType::None {
@@ -124,39 +137,74 @@ impl Lexer {
         self.register = "".to_string();
     }
 
+    fn get_default_state(&self, character: char) -> State {
+        if ENCAPSULATORS_LEFT.contains(&character) ||
+            ENCAPSULATORS_RIGHT.contains(&character) ||
+            OPERATORS.contains(&character)
+        {
+            // We are using the operator state here for encapsulators, despite the
+            // intuition that we should move to a special "scope" state. This is
+            // because encapsulators might also act as actual operators in a certain sense.
+            return State::Operator;
+        }
+
+        if SEPARATORS.contains(&character) {
+            return State::Separator;
+        }
+
+        if character.is_numeric() {
+            return State::Numeric;
+        }
+
+        if character.is_alphabetic() {
+            // If we are dealing with an alphabetic character, it must be the name
+            // of some variable, identifier, or keyword.
+            return State::Identifier;
+        }
+
+        match character {
+            ' ' => State::None,
+            '\'' => State::Character,
+            '"' => State::String,
+            ':' => State::Colon,
+            _ => {
+                error(format!("Invalid character '{}'", character).as_str());
+                exit(1);
+            }
+        }
+    }
+
     fn get_next_state(&self, character: char) -> State {
         match self.state {
-            State::None | State::Identifier => {
-                if
-                    ENCAPSULATORS_LEFT.contains(&character) ||
-                    ENCAPSULATORS_RIGHT.contains(&character) ||
-                    OPERATORS.contains(&character)
-                {
-                    // We are using the operator state here for encapsulators, despite the
-                    // intuition that we should move to a special "scope" state. This is
-                    // because encapsulators might also act as actual operators in a certain sense.
-                    return State::Operator;
+            State::None | State::Identifier | State::Separator => self.get_default_state(character),
+            State::Operator => {
+                // If we are in an operator state, we will accept more characters that look
+                // operator-like to form multi-character operators. For example +=, **, ->.
+                // But we also want to accept certain other characters while in thie state, such
+                // as > so that we can implement arrows like -> and =>. We should add those
+                // conditionals here rather than just going to the default state.
+                self.get_default_state(character)
+            },
+            State::Numeric => {
+                if character == '.' {
+                    // We are dealing with a decimal dot. We must make sure to parse this dot
+                    // as a decimal dot and not as something else such as an operator.
+                    return State::DecimalSeparator;
                 }
 
-                if SEPARATORS.contains(&character) {
-                    return State::Separator;
+                self.get_default_state(character)
+            },
+            State::DecimalSeparator | State::NumericDecimal => {
+                if character.is_numeric() {
+                    // We are dealing with the decimal part of a number.
+                    // Note that we do not go back to the Numeric state because it would allow
+                    // for illegal separator chaining such as 1.34.590 which is not a number.
+                    return State::NumericDecimal;
                 }
 
-                if character.is_alphabetic() {
-                    // If we are dealing with an alphabetic character, it must be the name
-                    // of some variable, identifier, or keyword.
-                    return State::Identifier;
-                }
-
-                match character {
-                    ' ' => State::None,
-                    '\'' => State::Character,
-                    '"' => State::String,
-                    ':' => State::Colon,
-                    _ => panic!(format!("Could not find a suitable state for character: '{}'", character))
-                }
-            }
-            _ => unimplemented!()
+                self.get_default_state(character)
+            },
+            _ => unimplemented!("{:?}", self.state)
         }
     }
 }
